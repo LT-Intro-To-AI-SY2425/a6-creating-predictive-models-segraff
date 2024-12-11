893d77fe44e60af9d2fa4547cd84534703fbf950
Sylvie Graff <segraff@cps.edu>
2024-12-11 10:59:21 -0600

    finished part 4

--- part3-multivariable-linear-regression/example2_linear_regression.py
+++ part3-multivariable-linear-regression/example2_linear_regression.py
@@ -1,55 +1,53 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
+from sklearn import linear_model
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
-'''
-**********CREATE THE MODEL**********
-'''
+data = pd.read_csv("part4-classification/suv_data.csv")
+data['Gender'].replace(['Male','Female'],[0,1],inplace=True)
 
-# imports the data and sets x and y values
-data = pd.read_csv("part3-multivariable-linear-regression/antelope_data.csv")
-print(data)
-x = data[["Adult Population", "Annual Precipitation", "Winter Severity"]].values
+x = data[["Age", "EstimatedSalary", "Gender"]].values
+y = data["Purchased"].values
+
+# Step 1: Print the values for x and y
 print(x)
-y = data["Fawn"].values
 print(y)
+# Step 2: Standardize the data using StandardScaler, 
+scaler = StandardScaler().fit(x)
+# Step 3: Transform the data
+x = scaler.transform(x)
 
-# separates the data into training and testing sets
-xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = .2)
-print(xtrain)
-print(xtest)
-# # reshape the xtrain data into a 2D array
-# xtrain = xtrain.reshape(-1, 1)
+# Step 4: Split the data into training and testing data
+x_train, x_test, y_train, y_test = train_test_split(x, y)
 
-# create the linear regression model using the training data
-model = LinearRegression().fit(xtrain, ytrain)
+# Step 5: Fit the data
 
-# get the coef_, intercept_ valuesm and r^2 values
-# use float() to turn the arrays into a single float value
-coef = np.around(model.coef_, 2)
-intercept = round(float(model.intercept_), 2)
-r_squared = round(model.score(x, y),2)
+# Step 6: Create a LogsiticRegression object and fit the data
+model = linear_model.LogisticRegression().fit(x_train, y_train)
 
-# print out the linear equation and r^2 value
-print(f"Model's Linear Equation: y={coef[0]}x1 + {coef[1]}x2 + {coef[2]}x3 + {intercept}")
-print("R Squared value:", r_squared)
+# Step 7: Print the score to see the accuracy of the model
+print("Accuracy:", model.score(x_test, y_test))
+print("*************")
+print("Testing Results:")
+print("")
+print(y_test)
+# Step 8: Print out the actual ytest values and predicted y values
+# based on the xtest data
+for index in range(len(x_test)):
+    x = x_test[index]
+    x = x.reshape(-1, 3)
+    print(x)
+    y_pred = int(model.predict(x))
 
-'''
-**********TEST THE MODEL**********
-'''
-
-# get the predicted y values for the xtest values - returns an array of the results
-predict = model.predict(xtest)
-# round the value in the np array to 2 decimal places
-predict = np.around(predict, 2)
-print(predict)
-
-# compare the actual and predicted values
-print("\nTesting Multivariable Model with Testing Data:")
-for index in range(len(xtest)):
-    actual = ytest[index] # gets the actual y value from the ytest dataset
-    predicted_y = predict[index] # gets the predicted y value from the predict variable
-    x_coord = xtest[index] # gets the x value from the xtest dataset
-    print(f"Adult Population: {x_coord[0]} Annual Percipitation: {x_coord[1]} Winter Severity: {x_coord[2]} Actual: {actual} Predicted: {predicted_y}")
+    if y_pred == 0:
+        y_pred = "Male"
+    else:
+        y_pred = "Female"
+    
+    actual = y_test[index]
+    if actual == 0:
+        actual = "Male"
+    else:
+        actual = "Female"
+    print("Predicted Gender: " + y_pred + " Actual Gender : " + actual)
+    print("")
