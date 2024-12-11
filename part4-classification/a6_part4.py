2c5988755870b71c1163ce1f33e02d22fa40d38d
Sylvie Graff <segraff@cps.edu>
2024-12-11 10:57:48 -0600

    finished part 4

--- part4-classification/a6_part4.py
+++ part4-classification/a6_part4.py
@@ -9,16 +9,21 @@ data['Gender'].replace(['Male','Female'],[0,1],inplace=True)
 x = data[["Age", "EstimatedSalary", "Gender"]].values
 y = data["Purchased"].values
 
-# Step 1: Print the values for x and y
+print(x)
+print(y)
 
-# Step 2: Standardize the data using StandardScaler, 
+scaler = StandardScaler().fit(x)
 
-# Step 3: Transform the data
+x=scaler.transform(x)
 
 # Step 4: Split the data into training and testing data
-
+x_train,x_test,y_train,y_test = train_test_split(x,y)
 # Step 5: Fit the data
-
+print("Accuracy:", model.score(x_test)):
+x = x_test[index]
+x = x.reshape(-1,3)
+print(x)
+y_pred = int(model.)
 # Step 6: Create a LogsiticRegression object and fit the data
 
 # Step 7: Print the score to see the accuracy of the model
